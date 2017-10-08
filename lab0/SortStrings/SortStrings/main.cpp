34f4b57c0954b62e15fa19afc1539ef3a15981e7
AnnMir <32086686+AnnMir@users.noreply.github.com>
2017-10-08 14:35:48 +0700

    Add files via upload

--- lab0/SortStrings/SortStrings/main.cpp
+++ lab0/SortStrings/SortStrings/main.cpp
@@ -9,26 +9,31 @@ using namespace std;
 int main(int argc, char** argv){
 	string str;
 	std::list<string> Str;
-	ifstream file;
-	file.open("input.txt", ios::in | ios::binary);
-	if (!file) {
+	ifstream file(argv[1]);
+	ofstream out(argv[2]/*"output.txt"*/);
+	//file.open("input.txt", ios::in | ios::binary);
+	/*if (!file) {
 		cout << "File error - can't open to read data!";
 		cin.sync(); cin.get(); return 2;
+	}*/
+	if (argc != 3){
+		out << "WRONG input";
 	}
-	while (1){
-		if (file.eof()) break;
-		getline(file,str);
-		Str.push_back(str);
+	else{
+		while (getline(file, str)){
+			//if (file.eof()) break;
+			Str.push_back(str);
+		}
+		using namespace Strings;
+		sort_strings(&Str);
+
+		while (!Str.empty()){
+			out << Str.front() << "\n";
+			Str.pop_front();
+		}
 	}
-	using namespace Strings;
-	sort_strings(&Str);
-	ofstream out("output.txt");
-	while (!Str.empty()){
-		out<<Str.front() << "\n";
-		Str.pop_front();
-	}
-	file.close(); // Закрываем файл
-	out.close();
+	//file.close(); // Закрываем файл
+	//out.close();
 	_getch();
 	return 0;
 }
