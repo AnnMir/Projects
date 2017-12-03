f031785db45883ca35cf6024e407b5b2e250b295
AnnMir <32086686+AnnMir@users.noreply.github.com>
2017-12-04 00:10:43 +0700

    Add files via upload

---
+++ lab2_Game_Life/Game_Process.h
@@ -0,0 +1,26 @@
+#include "Field.h"
+#include "My_exception.h"
+#include <Windows.h>
+#include <conio.h>
+
+namespace Life{
+	class Game{
+	private:
+		Field field;
+		int steps;
+
+		void start();
+		void set(int, int);
+		void random_set(int);
+		void clear(int, int);
+		void back();
+		void step(int);
+		void reset();
+		void save(std::string);
+		void load(std::string);
+	public:
+		Game(int, int);
+		~Game();
+		void exec_command(std::string command);
+	};
+}
